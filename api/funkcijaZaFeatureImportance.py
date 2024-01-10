7e1fb65f21f068ef48aaa315e86d9c4d362171e7
Voidxx <voidx@net.hr>
2024-01-10 16:55:16 +0100

    added scripts for sensitivity analysis graphs

--- api/funkcijaZaFeatureImportance.py
+++ api/funkcijaZaFeatureImportance.py
@@ -13,7 +13,8 @@ from typing import List, Dict
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.neighbors import NearestNeighbors
 import numpy as np
-from sklearn.model_selection import GridSearchCV
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestRegressor
 
 app = FastAPI()
 
@@ -31,17 +32,38 @@ feature_cols=['explicit', 'mode', 'speechiness', 'key', 'acousticness',
               'instrumentalness', 'liveness', 'valence', 'tempo', 'duration_ms',
               'time_signature', 'year', 'energy', 'danceability', 'loudness',]
 
-# Assuming df is your DataFrame and feature_cols are your feature columns
 normalized_df = df[feature_cols].copy()
 
-# Fit a NearestNeighbors model to the data
 neighborsModel = NearestNeighbors(n_neighbors=1500, metric='cosine').fit(normalized_df)
 
-# Create a new column 'album_name' in the dataframe that combines 'name' and 'album'
 df['album_name'] = df['name'] + ' - ' + df['album']
 
-# Create a new series that maps song names along with their album names to their indices
 indices = pd.Series(df.index, index=df['album_name']).drop_duplicates()
+
+# Initialize a random forest regressor
+rf = RandomForestRegressor(random_state=42)
+
+# Train the model
+rf.fit(normalized_df, normalized_df.index)
+
+# Get feature importances
+importances = rf.feature_importances_
+
+# Sort the feature importance in descending order
+sorted_indices = np.argsort(importances)[::-1]
+
+# Print the feature ranking
+print("Feature ranking:")
+for f in range(normalized_df.shape[1]):
+ print("%d. %s (%f)" % (f + 1, normalized_df.columns[sorted_indices[f]], importances[sorted_indices[f]]))
+
+# Plot the feature importance
+plt.figure()
+plt.title('Feature Importance')
+plt.bar(range(normalized_df.shape[1]), importances[sorted_indices], align='center')
+plt.xticks(range(normalized_df.shape[1]), normalized_df.columns[sorted_indices], rotation=90)
+plt.tight_layout()
+plt.show()
 
 # Mongo id schema
 class PyObjectId(ObjectId):
@@ -114,97 +136,71 @@ class SongTitles(BaseModel):
 
 @app.post("/suggest_songs", response_model=List[SuggestionModel])
 def generate_recommendation(song_titles: SongTitles):
-  # Initialize an empty list to store the scores
-  najslicnijiIndeksi = []
 
-  # Create a vector representation for each song
-  song_vectors = df[feature_cols].values
+ avg_sim_values = []
 
-  neighborsModel = NearestNeighbors()
-  param_grid = {'n_neighbors': [3, 10, 30, 50, 100, 200]}
-  grid_search = GridSearchCV(neighborsModel, param_grid, cv=5)
-  grid_search.fit(normalized_df)
-  print(grid_search.best_params_)
-  neighborsModel.fit(normalized_df)
-  print("Model fitted successfully.")
-
-  # Find the nearest neighbors for each song
-  for song_title, album_title in zip(song_titles.song_titles, song_titles.albums):
-    full_title = song_title + ' - ' + album_title
-    if full_title not in indices.index:
-        print(f"No song titled '{full_title}' found.")
+ for num_neighbors in range(2, 5):
+    neighborsModel = NearestNeighbors(n_neighbors=num_neighbors)
+    neighborsModel.fit(normalized_df)
+    najslicnijiIndeksi = []
+    for song_title, album_title in zip(song_titles.song_titles, song_titles.albums):
+        full_title = song_title + ' - ' + album_title
+        if full_title not in indices.index:
+            print(f"No song titled '{full_title}' found.")
+        else:
+            print(f"Found song titled '{full_title}'.")
+            index = indices[full_title]
+            dist, ind = neighborsModel.kneighbors(normalized_df.iloc[index].values.reshape(1, -1), n_neighbors=num_neighbors)
+            najslicnijiIndeksi.extend(ind[0])
+    najslicnijiIndeksi = list(set(najslicnijiIndeksi))
+    najslicnijiVektori = normalized_df[normalized_df.index.isin(najslicnijiIndeksi)]
+    najslicnijiVektori = najslicnijiVektori.to_string(header=False, index=False)
+    najslicnijePjesme = df[df.index.isin(najslicnijiIndeksi)]
+    najslicnijiVektori = najslicnijiVektori.strip()
+    lines = najslicnijiVektori.split('\n')
+    vektori = [line.split() for line in lines]
+    vektori = [[float(value) for value in row] for row in vektori]
+    matrica_vektora = np.array(vektori)
+    sim = cosine_similarity(matrica_vektora)
+    indeksi = []
+    for song_title in song_titles.song_titles:
+        for idx, item in enumerate(najslicnijePjesme.iterrows()):
+            if item[1]['name'].strip() == song_title:
+              indeksi.append(idx)
+              break
+    avg_sim = [0] * len(najslicnijePjesme)
+    for index in indeksi:
+        for col, value in enumerate(sim[index]):
+            avg_sim[col] += value / len(indeksi)
+    avg_sim = list(enumerate(avg_sim))
+    avg_sim = sorted(avg_sim, key = lambda x: x[1], reverse = True)
+    avg_sim = [sim_pair for sim_pair in avg_sim if sim_pair[0] not in indeksi]
+    if len(avg_sim) > 0:
+      avg_sim = sum(avg_sim[i][1] for i in range(len(avg_sim))) / len(avg_sim)
     else:
-          print(f"Found song titled '{full_title}'.")
-
-          # Get song index
-          index = indices[full_title]
-          # Calculate the score for the song
-          dist, ind = neighborsModel.kneighbors(normalized_df.iloc[index].values.reshape(1, -1), n_neighbors=1500)
-          najslicnijiIndeksi.extend(ind[0])
-          print(len(najslicnijiIndeksi))
-
-  # Filter out the songs from song_titles
-  najslicnijiIndeksi = list(set(najslicnijiIndeksi))
-  najslicnijiVektori = normalized_df[normalized_df.index.isin(najslicnijiIndeksi)]
-  najslicnijiVektori = najslicnijiVektori.to_string(header=False, index=False)
-  najslicnijePjesme = df[df.index.isin(najslicnijiIndeksi)]
-
-  # Remove leading and trailing whitespace
-  najslicnijiVektori = najslicnijiVektori.strip()
- 
-  # Split the string into lines
-  lines = najslicnijiVektori.split('\n')
- 
-  # Split each line into a list of values
-  vektori = [line.split() for line in lines]
-
-  # Convert each value in the vektori to float
-  vektori = [[float(value) for value in row] for row in vektori]
-
-  # Convert your list of vectors to a 2D numpy array
-  matrica_vektora = np.array(vektori)
-
-  # Compute cosine similarity matrix
-  sim = cosine_similarity(matrica_vektora)
-
- 
+      avg_sim = 0
+    avg_sim_values.append(avg_sim)
 
 
-
-  # Find indices of favorites in the most_similar_movies list
-  indeksi = []
-  for song_title in song_titles.song_titles:
-      for idx, item in enumerate(najslicnijePjesme.iterrows()):
-        if item[1]['name'].strip() == song_title:
-          indeksi.append(idx)
-          break
-
-  # initialize a new list of zeros, length of movies / row in matrix
-  avg_sim = [0] * len(najslicnijePjesme)
-
-  # add values from each favorite movie 's similarity vector and calculate average
-  for index in indeksi:
-      for col, value in enumerate(sim[index]):
-        avg_sim[col] += value / len(indeksi)
-
-  # create pairs of movie index and calculated average similarity
-  avg_sim = list(enumerate(avg_sim))
-  avg_sim = sorted(avg_sim, key = lambda x: x[1], reverse = True)# Remove original picks
-  avg_sim = [sim_pair
-      for sim_pair in avg_sim
-        if sim_pair[0] not in indeksi
-  ]
-
-  most_similar = avg_sim[0:15]
+ # Plot the average similarity values
+ plt.figure(figsize=(10, 6))
+ plt.plot(range(len(avg_sim_values)), avg_sim_values, marker='o')
+ plt.xlabel('Number of Neighbors')
+ plt.ylabel('Average Similarity')
+ plt.title('Average Similarity vs Number of Neighbors')
+ plt.grid(True)
+ plt.show()
 
 
-  recommendations = []
-
-  for i in range(len(most_similar)):
-    row = najslicnijePjesme.iloc[most_similar[i][0]]
+ recommendations = []
+ for i in range(len(avg_sim_values)):
+    row = najslicnijePjesme.iloc[avg_sim_values[i][0]]
     song_name, song_album, song_artists = row['name'], row['album'], row['artists']
     artist_objects = [Artist(name=artist['name'], id=artist['id']) for artist in song_artists]
     recommendation = SuggestionModel(name=song_name, album=song_album, artists=artist_objects)
     recommendations.append(recommendation)
+ return recommendations
 
-  return recommendations
+
+
+
